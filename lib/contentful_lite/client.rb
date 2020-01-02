ed380b1126e6912d9977a469f4ca2f2e2a31d0ae
Gonzalo Muñoz <gonzalo.munoz@juul.com>
2020-01-02 12:49:31 -0300

    Add initializer for client class

--- lib/contentful_lite/client.rb
+++ lib/contentful_lite/client.rb
@@ -2,5 +2,13 @@ require 'http'
 
 module ContentfulLite
   class Client
+    attr_reader :space_id, :environment, :preview
+
+    def initialize(space_id:, access_token:, environment: nil, preview: false)
+      @space_id = space_id
+      @environment = environment
+      @preview = preview
+      @access_token = access_token
+    end
   end
 end
