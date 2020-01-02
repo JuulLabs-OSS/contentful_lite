32d2ad207ca25bd4f71405d751094d4e3544dafe
Gonzalo Muñoz <gonzalo.munoz@juul.com>
2020-01-02 12:59:05 -0300

    Add initial handling of response

--- lib/contentful_lite/client.rb
+++ lib/contentful_lite/client.rb
@@ -2,6 +2,13 @@ require 'http'
 
 module ContentfulLite
   class Client
+    class RequestError < StandardError
+      def initialize(response, body)
+        @response = response
+        @body = body
+      end
+    end
+    class NotFoundError < RequestError; end
     attr_reader :space_id, :environment, :preview
 
     def initialize(space_id:, access_token:, environment: nil, preview: false)
@@ -23,6 +30,11 @@ module ContentfulLite
       parameters.transform_keys!(&:to_s)
       parameters.transform_values{ |value| value.is_a?(::Array) ? value.join(',') : value }
       response = HTTP[request_headers].get(create_url(endpoint), params: parameters)
+      body = response.to_s
+      body = Zlib::GzipReader.new(StringIO.new(body)).read if response.headers['Content-Encoding'].eql?('gzip')
+      JSON.parse(body).tap do |parsed|
+        raise error_class(response.status).new(response, parsed) if response.status != 200
+      end
     end
 
     def request_headers
@@ -31,6 +43,10 @@ module ContentfulLite
         'Content-Type' => 'application/vnd.contentful.delivery.v1+json',
         'Accept-Encoding' => 'gzip'
       }
+    end
+
+    def error_class(status)
+      status == 404 ? NotFoundError : RequestError
     end
   end
 end
