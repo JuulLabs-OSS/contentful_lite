7cdcb302cfcbf826fa77bb211ad7d1a26ad77e60
Gonzalo Muñoz <gonzalo.munoz@juul.com>
2020-01-02 12:52:21 -0300

    Add methods for creating the URL and headers for a contentful request

--- lib/contentful_lite/client.rb
+++ lib/contentful_lite/client.rb
@@ -10,5 +10,21 @@ module ContentfulLite
       @preview = preview
       @access_token = access_token
     end
+
+    private
+
+    def create_url(endpoint)
+      base = "https://#{preview ? 'preview' : 'cdn'}.contentful.com/spaces/#{space_id}/" +
+        ( environment.nil? ? '' : "environments/#{environment}/" ) +
+        endpoint.to_s
+    end
+
+    def request_headers
+      {
+        'Authorization' => "Bearer #{@access_token}",
+        'Content-Type' => 'application/vnd.contentful.delivery.v1+json',
+        'Accept-Encoding' => 'gzip'
+      }
+    end
   end
 end
